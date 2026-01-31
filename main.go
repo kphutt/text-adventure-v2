ac2c2b6023e2a691bd527d89369e463bbece9b07
Karsten Huttelmaier <karsten.huttelmaier@gmail.com>
2026-01-30 22:09:17 -0800

    Refactor: Improve game UI layout and add win screen

--- main.go
+++ main.go
@@ -30,20 +30,28 @@ func main() {
 	for {
 		screen.Clear()
 		y := 0
-		for _, line := range strings.Split(g.Look(), "\n") {
+
+		helpMessage, _ := g.HandleCommand("help")
+		for _, line := range strings.Split(helpMessage, "\n") {
 			drawText(screen, 0, y, tcell.StyleDefault, line)
 			y++
 		}
-		y = 10
+
+		y++
 		for _, line := range strings.Split(g.GetMapString(), "\n") {
 			drawText(screen, 0, y, tcell.StyleDefault, line)
 			y++
 		}
+		
 		y++
-		helpMessage, _ := g.HandleCommand("help")
-		drawText(screen, 0, y, tcell.StyleDefault, helpMessage)
-		drawText(screen, 0, 20, tcell.StyleDefault, "> "+inputStr)
-		drawText(screen, 0, 15, tcell.StyleDefault, message)
+		for _, line := range strings.Split(g.Look(), "\n") {
+			drawText(screen, 0, y, tcell.StyleDefault, line)
+			y++
+		}
+		
+		drawText(screen, 0, y, tcell.StyleDefault, message)
+		y++
+		drawText(screen, 0, y, tcell.StyleDefault, "> "+inputStr)
 		screen.Show()
 
 		ev := screen.PollEvent()
@@ -56,7 +64,17 @@ func main() {
 			case tcell.KeyEnter:
 				message, shouldExit = g.HandleCommand(inputStr)
 				if shouldExit {
-					return
+					screen.Clear()
+					drawText(screen, 0, 0, tcell.StyleDefault, message)
+					drawText(screen, 0, 1, tcell.StyleDefault, "Press any key to exit.")
+					screen.Show()
+					// wait for any key press
+					for {
+						ev := screen.PollEvent()
+						if _, ok := ev.(*tcell.EventKey); ok {
+							return
+						}
+					}
 				}
 				inputStr = ""
 			case tcell.KeyBackspace, tcell.KeyBackspace2:
@@ -69,7 +87,17 @@ func main() {
 				case 'w', 'a', 's', 'd', 'e', 'i', 'u', 'h', 'q':
 					message, shouldExit = g.HandleCommand(string(ev.Rune()))
 					if shouldExit {
-						return
+						screen.Clear()
+						drawText(screen, 0, 0, tcell.StyleDefault, message)
+						drawText(screen, 0, 1, tcell.StyleDefault, "Press any key to exit.")
+						screen.Show()
+						// wait for any key press
+						for {
+							ev := screen.PollEvent()
+							if _, ok := ev.(*tcell.EventKey); ok {
+								return
+							}
+						}
 					}
 				default:
 					inputStr += string(ev.Rune())
